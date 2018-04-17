885155370be4c75d1fb8235e59c10750d265602a
nagarajug <nagarajug@nagarajug.BHELRD.IN>
2018-04-17 18:20:02 +0530

    completed 9th chapter

--- src/com/simpleprogrammer/Program.java
+++ src/com/simpleprogrammer/Program.java
@@ -1,5 +1,7 @@
 package com.simpleprogrammer;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 
 public class Program {
@@ -12,7 +14,16 @@ public class Program {
 
 		User user = new User();
 		user.setName("Joe");
-		user.setGoal(250);
+		//user.getHistory().add(new UserHistory(new Date(), "set name to Joe"));
+		user.addHistory(new UserHistory(new Date(), "set name to Joe"));
+		//user.getHistory().put("GL123", new UserHistory(new Date(), "set name to Joe"));
+		user.getProteinData().setGoal(250);
+		//user.getHistory().add(new UserHistory(new Date(), "set the goal to 250"));
+		//user.getHistory().put("GL124",new UserHistory(new Date(), "set the goal to 250"));
+		user.addHistory(new UserHistory(new Date(), "set the goal to 250"));
+		//user.setGoalAlert(new GoalAlert("Congractualtions!"));
+		user.getGoalAlerts().add(new GoalAlert("Congractualtions!"));
+		user.getGoalAlerts().add(new GoalAlert("You did it!"));
 		session.save(user);
 
 		session.getTransaction().commit();
@@ -21,13 +32,25 @@ public class Program {
 
 		User loadedUser = (User) session.get(User.class, user.getId());
 		System.out.println(loadedUser.getName());
-		System.out.println(loadedUser.getGoal());
+		System.out.println(loadedUser.getProteinData().getGoal());
+		for (UserHistory history : loadedUser.getHistory()){
+		    System.out.println(history.getEntryTime() + " " + history.getEntry());
+		}
+		/*for (Entry<String, UserHistory> history : loadedUser.getHistory().entrySet()){
+		    System.out.println("Key value: " + history.getKey());
+            System.out.println(history.getValue().getEntryTime() + " " + history.getValue().getEntry());
+        }*/
 		
-		loadedUser.setTotal(loadedUser.getTotal() + 50);
-
+		loadedUser.getProteinData().setTotal(loadedUser.getProteinData().getTotal() + 50);
+		//user.getHistory().add(new UserHistory(new Date(), "added 50 protein"));
+		//user.getHistory().put("GL125", new UserHistory(new Date(), "added 50 protein"));
+		user.addHistory(new UserHistory(new Date(), "added 50 protein"));
+		user.setProteinData(new ProteinData());
+		
 		session.getTransaction().commit();
 
-/*		session.beginTransaction();
+		/*		
+        session.beginTransaction();
 
 		User loadedUser1 = (User) session.get(User.class, 2);
 		System.out.println(loadedUser1.getName());
@@ -38,14 +61,14 @@ public class Program {
 		session.close();
 
 		session.beginTransaction();
-*/
-		/*User loadedUser3 = (User) session.load(User.class, 2);
+		 */
+		/*
+		User loadedUser3 = (User) session.load(User.class, 2);
 		System.out.println(loadedUser3.getName());
 		System.out.println(loadedUser3.getGoal());
 
-		session.getTransaction().commit();*/
-
+		session.getTransaction().commit();
+		*/
 		HibernateUtilities.getSessionFactory().close();
 	}
-
 }
--- src/com/simpleprogrammer/User.java
+++ src/com/simpleprogrammer/User.java
@@ -1,11 +1,26 @@
 package com.simpleprogrammer;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User {
 	
 	private int id;
 	private String name;
-	private int total;
-	private int goal;
+	//private ProteinData proteinData = new ProteinData();
+	private ProteinData proteinData;
+	//private Set<UserHistory> history = new HashSet<UserHistory>();
+	private List<UserHistory> history = new ArrayList<UserHistory>();
+	//private Map<String, UserHistory> history = new HashMap<String, UserHistory>();
+	//private Collection<UserHistory> history = new ArrayList<UserHistory>();
+	//private GoalAlert goalAlert;
+	private Set<GoalAlert> goalAlerts = new HashSet<GoalAlert>();
+	public User() {
+	    setProteinData(new ProteinData());
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -18,16 +33,61 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getTotal() {
-		return total;
-	}
-	public void setTotal(int total) {
-		this.total = total;
-	}
-	public int getGoal() {
-		return goal;
-	}
-	public void setGoal(int goal) {
-		this.goal = goal;
-	}
+    public ProteinData getProteinData() {
+        return proteinData;
+    }
+    public void setProteinData(ProteinData proteinData) {
+        this.proteinData = proteinData;
+        proteinData.setUser(this);
+    }
+    /*public Set<UserHistory> getHistory()
+    {
+        return history;
+    }
+    public void setHistory(Set<UserHistory> history)
+    {
+        this.history = history;
+    }*/
+    public List<UserHistory> getHistory()
+    {
+        return history;
+    }
+    public void setHistory(List<UserHistory> history)
+    {
+        this.history = history;
+    }
+    /*public Map<String, UserHistory> getHistory(){
+        return history;
+    }
+    public void setHistory(Map<String, UserHistory> history){
+        this.history = history;
+    }*/
+    /*public Collection<UserHistory> getHistory(){
+        return history;
+    }
+    public void setHistory(Collection<UserHistory> history){
+        this.history = history;
+    }*/
+    
+    public void addHistory(UserHistory historyItem) {
+        historyItem.setUser(this);
+        history.add(historyItem);
+    }
+
+    /*public GoalAlert getGoalAlert(){
+        return goalAlert;
+    }
+
+    public void setGoalAlert(GoalAlert goalAlert){
+        this.goalAlert = goalAlert;
+    }*/
+
+    public Set<GoalAlert> getGoalAlerts(){
+        return goalAlerts;
+    }
+
+    public void setGoalAlerts(Set<GoalAlert> goalAlerts){
+        this.goalAlerts = goalAlerts;
+    }
+    
 }
